a4a3730a371f0d82585bb7f8baa2b78fa54e75d9
Celebrinborn <cameronjunkmail1@gmail.com>
2026-02-18 13:09:55 -0800

    migrating to new hostname for api

--- src/frontend/src/config/app.config.ts
+++ src/frontend/src/config/app.config.ts
@@ -76,7 +76,7 @@ const CONFIGS: Record<Environment, AppConfig> = {
 
   prod: {
     env: "prod",
-    apiBaseUrl: "",
+    apiBaseUrl: "https://api.dnd.cameron.land",
     storage: {
       containerMaps: "maps",
       containerCharacters: "characters",
